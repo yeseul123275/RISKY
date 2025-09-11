1743b8428da653b436a29c72a2f4b63a7ffa26b8
yeseul123275 <jys11034@gmail.com>
2025-09-11 18:10:18 +0900

    ye

--- theme/theme_wide_17/footer.php
+++ theme/theme_wide_17/footer.php
@@ -1,22 +1,37 @@
-	<footer class="py-5 bg-dark">
+	<footer class="py-5 footer_wrap position-relative">
+
 		<div class="container footer">
 			<div class="row">
-				<div class="col-md-12 text-white text-center">
-					<h2 class="en1">ETY.KR</h2><!-- image or text  -->
-					<p class="ks2 f12">
-					무료테마인 <a href="http://ety.kr" target="_blank" class="color-white">에티테마</a>는 상업적으로 이용하셔도 됩니다. 하지만 배포, 재배포는 안됩니다. <br />
-					또한 해당 테마에 대해서 지적재산권을 주장 할 수 없음을 미리 알려드립니다. <br />
-					별도 문의사항은 <span class="color-white">mail@mail.com</span> 으로 연락 주시기 바랍니다.
-					</p>
-					<p class="ks2 f12">
-						<i class="far fa-building"></i> 사무실 : 경기도 아름다운 아파트 101동 1004호<br />
-						<i class="fas fa-phone"></i> 연락처 : 010-0000-0000<br />
-						<i class="far fa-envelope-open"></i> <a href="mailto:mail@mail.com" class="color-white">Email : mail@mail.com</a><br />
-						<i class="fas fa-fax"></i> 팩스번호 : 02) 1234-1234<br />
-					</p>
-
+				<div class="col-md-12 d-flex gap-4 align-items-center">
+					
+					<h2 class="en1">
+						<img src="/0813/img/ys_logo.png" >
+					</h2><!-- image or text  -->
+					<div >
+						<ul class='d-flex gap-3 fw700'>
+							<li>
+								<a href="" class='text-black text-decoration-none'>YOUTUBE</a>
+							</li>
+							<li>
+								<a href="" class='text-black text-decoration-none'>INSTAGRAM</a>
+							</li>
+						</ul>
+						
+						
+					</div>
+					
 				</div>
+				
+				<p class="footer_info">
+				주식회사: 리스키 | OWNER: 홍유경  |  BUSINESS NUMBER: 879-81-03358  |   MAIL-ORDER LICENSE: 2024-서울 용산-0835<br>
+				E-MAIL: help@riskybeauty.com  |  ADDRESS: 04404 서울특별시 용산구 이태원로 220 (한남동) 3층  |  T.02-6223-5988
+				</p>
+				<p class="copyright mb-0">
+					Copyright 2025. MONCASA All Rights Reserved.
+				</p>
+
 			</div>
 		</div><!--/container-->
+
     </footer>
 	
--- theme/theme_wide_17/head.sub.php
+++ theme/theme_wide_17/head.sub.php
@@ -106,7 +106,7 @@ if(!defined('G5_IS_ADMIN'))
 <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
 
 <!-- Bootstrap core CSS -->
-<link href="<?php echo G5_THEME_URL?>/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo G5_THEME_URL?>/assets/bootstrap/css/bootstrap.css" rel="stylesheet">
 <!-- fontawesome -->
 <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <!-- owl Carousel -->
@@ -120,7 +120,7 @@ if(!defined('G5_IS_ADMIN'))
 <link href="<?php echo G5_THEME_URL?>/css/animate.css" rel="stylesheet">
 <link href="<?php echo G5_THEME_URL?>/css/bootstrap-dropdownhover.css" rel="stylesheet">
 <!-- Custom & ety -->
-<link href="<?php echo G5_THEME_URL?>/css/modern-business.css" rel="stylesheet">
+<link href="<?php echo G5_THEME_URL?>/css/modern-business.css?ver=<?php echo time(); ?>" rel="stylesheet">
 <link href="<?php echo G5_THEME_URL?>/css/ety.css" rel="stylesheet">
 
 
